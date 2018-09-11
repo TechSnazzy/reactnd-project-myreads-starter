d5204fd6ba44f9f1917452eccb493313b54b9af4
Sean Morrison <sean1972@gmail.com>
2018-09-10 20:44:11 -0700

    Removed last bit of code from App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,6 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-
-        ) : (
-
-        )}
       </div>
     )
   }
