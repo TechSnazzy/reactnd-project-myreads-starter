8c862f700cef86ab4579f50f3ea10e64b07d3c4c
Sean Morrison <sean1972@gmail.com>
2018-09-10 21:44:21 -0700

    Remove the state because we don't have any state at the moment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import MainPage from './MainPage.js';
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
