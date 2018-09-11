2237f0a98349b6f78e89b1c24238cc386e37a537
Sean Morrison <sean1972@gmail.com>
2018-09-10 21:27:10 -0700

    Import SearchPage and Mainpage Components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 // import * as BooksAPI from './BooksAPI'
+import SearchPage from './SearchPage.js';
+import MainPage from './MainPage.js';
 import './App.css'
 
 class BooksApp extends React.Component {
